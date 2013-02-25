8682af95107d18ecb105a66b3ad947e2519570e8
Markus Teich <teichm@fs.tum.de>
2013-02-25 22:23:47 +0100

    change modkey back to super

--- config.h
+++ config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
