0ddd0866e503f05dbb895e2ffa65c701589537de
Markus Teich <markus.teich@stusta.mhn.de>
2013-03-01 01:29:28 +0100

    evolute keymappings, add lock, fix whitespace

--- config.h
+++ config.h
@@ -31,17 +31,17 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "Æ", "À", "Á", "Å", "Â", "Ã", "7", "8", "9" };
+static const char *tags[] = { "Æ[c]", "À[s]", "Á[n]", "Å[m]", "Â[d]", "Ã[o]", "K", "H", "G" };
 
 static const Rule rules[] = {
-	/* class           instance    title       tags mask     isfloating   resizehints   monitor */
-	{ "Gimp",          NULL,       NULL,       1 << 5,       True,        False,        -1 },
-	{ "Firefox",       NULL,       NULL,       1 << 1,       False,       False,        -1 },
-	{ "Iceweasel",     NULL,       NULL,       1 << 1,       False,       False,        -1 },
-	{ "Pidgin",        NULL,       NULL,       1 << 2,       False,       False,        -1 },
-	{ "Thunderbird",   NULL,       NULL,       1 << 2,       False,       False,        -1 },
-	{ "Gmusicbrowser", NULL,       NULL,       1 << 3,       False,       False,        -1 },
-	{ "MPlayer",       NULL,       NULL,       0,            False,       True,         -1 },
+	/* class           inst  title  tags    floating  resize  monitor */
+	{ "Gimp",          NULL, NULL,  1 << 5, True,     False,  -1 },
+	{ "Firefox",       NULL, NULL,  1 << 1, False,    False,  -1 },
+	{ "Iceweasel",     NULL, NULL,  1 << 1, False,    False,  -1 },
+	{ "Pidgin",        NULL, NULL,  1 << 2, False,    False,  -1 },
+	{ "Thunderbird",   NULL, NULL,  1 << 2, False,    False,  -1 },
+	{ "Gmusicbrowser", NULL, NULL,  1 << 3, False,    False,  -1 },
+	{ "MPlayer",       NULL, NULL,  0,      False,    True,   -1 },
 };
 
 /* layout(s) */
@@ -64,61 +64,62 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG],
-															"-nf", colors[0][ColFG],
-															"-sb", colors[1][ColBG],
-															"-sf", colors[1][ColFG], NULL };
-
-static const char *termcmd[]  = { "urxvtc", NULL };
-static const char *quitcmd[]  = { "killall", "dwm", NULL };
-
-static const char *musicprev[] = {"gmusicbrowser", "-cmd", "PrevSong", NULL};
-static const char *musicstop[] = {"gmusicbrowser", "-cmd", "Stop", NULL};
-static const char *musicplay[] = {"gmusicbrowser", "-cmd", "PlayPause", NULL};
-static const char *musicnext[] = {"gmusicbrowser", "-cmd", "NextSong", NULL};
-static const char *wallpaper[] = {"bin/wallpaper_next", NULL};
+static const char *dmenucmd[]  = {"dmenu_run", "-fn", font,
+                                  "-nb", colors[0][ColBG],
+                                  "-nf", colors[0][ColFG],
+                                  "-sb", colors[1][ColBG],
+                                  "-sf", colors[1][ColFG],                 NULL};
+static const char *termcmd[]   = {"urxvtc",                                NULL};
+static const char *quitcmd[]   = {"killall", "dwm",                        NULL};
+static const char *lockcmd[]   = {"i3lock", "-i", "bilder/wallpaper/lock", NULL};
+static const char *musicprev[] = {"gmusicbrowser", "-cmd", "PrevSong",     NULL};
+static const char *musicstop[] = {"gmusicbrowser", "-cmd", "Stop",         NULL};
+static const char *musicplay[] = {"gmusicbrowser", "-cmd", "PlayPause",    NULL};
+static const char *musicnext[] = {"gmusicbrowser", "-cmd", "NextSong",     NULL};
+static const char *wallpaper[] = {"bin/wallpaper_next",                    NULL};
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_F5,     spawn,          {.v = musicprev } },
-	{ MODKEY,                       XK_F6,     spawn,          {.v = musicstop } },
-	{ MODKEY,                       XK_F7,     spawn,          {.v = musicplay } },
-	{ MODKEY,                       XK_F8,     spawn,          {.v = musicnext } },
-	{ MODKEY,                       XK_F9,     spawn,          {.v = wallpaper } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = quitcmd } },
+	/* modifier         key        function        argument */
+	{ MODKEY|ShiftMask, XK_r,      quit,           {0} },
+	{ MODKEY|ShiftMask, XK_q,      spawn,          {.v = quitcmd } },
+	{ MODKEY,           XK_q,      spawn,          {.v = lockcmd } },
+	{ MODKEY,           XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,           XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY,           XK_b,      togglebar,      {0} },
+	{ MODKEY,           XK_a,      focusstack,     {.i = +1 } },
+	{ MODKEY,           XK_l,      focusstack,     {.i = -1 } },
+	{ MODKEY,           XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,           XK_e,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask, XK_i,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask, XK_e,      setmfact,       {.f = +0.05} },
+	{ MODKEY,           XK_space,  zoom,           {0} },
+	{ MODKEY,           XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask, XK_x,      killclient,     {0} },
+	{ MODKEY|ShiftMask, XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask, XK_y,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask, XK_f,      setlayout,      {.v = &layouts[2]} },
+//	{ MODKEY,           XK_bla,    setlayout,      {0} },
+	{ MODKEY|ShiftMask, XK_space,  togglefloating, {0} },
+	{ MODKEY,           XK_u,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask, XK_u,      tag,            {.ui = ~0 } },
+	{ MODKEY,           XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,           XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask, XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask, XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,           XK_F5,     spawn,          {.v = musicprev } },
+	{ MODKEY,           XK_F6,     spawn,          {.v = musicstop } },
+	{ MODKEY,           XK_F7,     spawn,          {.v = musicplay } },
+	{ MODKEY,           XK_F8,     spawn,          {.v = musicnext } },
+	{ MODKEY,           XK_F9,     spawn,          {.v = wallpaper } },
+	TAGKEYS(            XK_c,                      0)
+	TAGKEYS(            XK_s,                      1)
+	TAGKEYS(            XK_n,                      2)
+	TAGKEYS(            XK_m,                      3)
+	TAGKEYS(            XK_d,                      4)
+	TAGKEYS(            XK_o,                      5)
+	TAGKEYS(            XK_k,                      6)
+	TAGKEYS(            XK_h,                      7)
+	TAGKEYS(            XK_g,                      8)
 };
 
 /* button definitions */
